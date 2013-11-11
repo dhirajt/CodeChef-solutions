09b3234122b5290c8a56a92d342f8a5f96c41f04
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-11-11 16:38:44 +0530

    pep8 fix generate.py

--- generate.py
+++ generate.py
@@ -7,9 +7,10 @@ import datetime
 
 from jinja2 import Template
 
+
 def generate_project_dir(context):
-    template_dir = [ item for item in os.listdir('.')
-                     if '{{' in item ][0]
+    template_dir = [item for item in os.listdir('.')
+                    if '{{' in item][0]
 
     dir_name = Template(template_dir)
     dir_name = dir_name.render(**context)
@@ -17,17 +18,20 @@ def generate_project_dir(context):
     os.mkdir(dir_name)
 
     ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-    TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),template_dir)
-    PROJECT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),dir_name)
+    TEMPLATE_PATH = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), template_dir)
+    PROJECT_PATH = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), dir_name)
 
-    files = [ name for name in os.listdir(TEMPLATE_PATH) if os.path.isfile(os.path.join(TEMPLATE_PATH, name))]
+    files = [name for name in os.listdir(
+        TEMPLATE_PATH) if os.path.isfile(os.path.join(TEMPLATE_PATH, name))]
 
     for in_file in files:
         in_file = os.path.join(TEMPLATE_PATH, in_file)
         out_file = Template(os.path.join(PROJECT_PATH, in_file))
         out_file = out_file.render(**context)
 
-        with open(in_file) as f, open(out_file,'w') as f1:
+        with open(in_file) as f, open(out_file, 'w') as f1:
             template = Template(f.read())
             rendered_content = template.render(**context)
             f1.write(rendered_content)
@@ -39,6 +43,3 @@ if __name__ == '__main__':
         'time': datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
     }
     generate_project_dir(context)
-
-
-
