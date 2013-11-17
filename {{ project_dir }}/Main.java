09ae68683cdfb9336f0e2f25a2b6e4f151d5b28a
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-11-17 12:37:07 +0530

    solved problem 42 inc,java,python

---
+++ {{ project_dir }}/Main.java
@@ -0,0 +1,15 @@
+/*
+
+    started at: {{ time }}
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        /* code */     
+    }
+}
