bf469a5a9a1b427bef694f046fbf4bd25d64c7ba
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-11-23 12:20:57 +0530

    solved turbosort in java

--- easy/turbosort/Main.java
+++ easy/turbosort/Main.java
@@ -5,11 +5,29 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import java.util.Arrays;
 
 public class Main{
     
     public static void main(String[] args) throws Exception{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        /* code */     
+        PrintWriter writer=new PrintWriter(new OutputStreamWriter(System.out));
+        
+        int iterations = Integer.parseInt(reader.readLine());
+        int array[] = new int[iterations];
+
+        for (int i=0; i<iterations; ++i) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        Arrays.sort(array);
+
+        for (int i=0; i<iterations; ++i) {
+            writer.println(array[i]);       
+        }   
+        writer.flush();
+        writer.close();
     }
 }
