acd4cca16a3b30fe3f36adeb9a65458cfb118cb0
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-11-24 01:22:31 +0530

    solved sumtriangle in java and c

--- easy/sumtriangle/Main.java
+++ easy/sumtriangle/Main.java
@@ -10,6 +10,44 @@ public class Main{
     
     public static void main(String[] args) throws Exception{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        /* code */     
+        
+        int iterations = Integer.parseInt(reader.readLine());
+        int lines = 0;
+
+        for (int it=0; it<iterations; it++) {
+            lines = Integer.parseInt(reader.readLine());
+
+            int triangle[][] = new int[lines][lines];
+
+            for (int i = 0; i < lines; ++i){
+                String temp[] = reader.readLine().split(" ");
+                
+                for (int j = 0; j < temp.length; ++j){
+                    triangle[i][j] = Integer.parseInt(temp[j]); 
+
+                }
+            }
+            //--------------------------------------
+
+            int row = lines - 2;
+            int col = 0;
+
+            while(row>=0){
+                col = 0;
+                while(col<=row){
+                    triangle[row][col] += max(triangle[row+1][col],triangle[row+1][col+1]);
+                    col++;
+                }
+                row--;
+            }
+            System.out.println(triangle[0][0]);
+        }
+        
+    }
+    public static int max(int a,int b){
+        if (a>b) {
+            return a;
+        }
+        return b;
     }
 }
--- easy/sumtriangle/sumtriangle.c
+++ easy/sumtriangle/sumtriangle.c
@@ -4,8 +4,46 @@
 
 #include <stdio.h>
 
+
+int max(int a,int b)
+{
+    return (a>b) ? a : b;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int triangle[100][100] = {{0}};
+    int iterations = 0;
+    int lines = 0;
+
+    scanf("%d",&iterations);
+    for (int it = 0; it < iterations; ++it)
+    {
+        scanf("%d",&lines);
+        for (int i = 0; i < lines; ++i)
+        {
+            for (int j = 0; j <= i; ++j)
+            {
+                scanf("%d",&triangle[i][j]);
+            }
+        }
+
+        //------------------------------
+
+        int row = lines - 2;
+        int col = 0;
+
+        while(row>=0)
+        {   col = 0;
+            while(col<=row)
+            {
+                triangle[row][col] += max(triangle[row+1][col],triangle[row+1][col+1]);
+                col++;
+            }
+            row--;
+        }
+        printf("%d\n",triangle[0][0]);
+        
+    }
     return 0;
 }
