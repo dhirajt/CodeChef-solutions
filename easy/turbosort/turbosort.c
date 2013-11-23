9f009f8a2c829b18b27ae73c1a044c59dff4dc20
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-11-23 16:59:13 +0530

    solved turbosort c

--- easy/turbosort/turbosort.c
+++ easy/turbosort/turbosort.c
@@ -3,9 +3,30 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int compare (const void * a, const void * b)
+{
+  return ( *(int*)a - *(int*)b );
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int arraysize = 0;
+    scanf("%d",&arraysize);
+    
+    int* array = (int *) calloc(arraysize,sizeof(int));
+
+    for (int i = 0; i < arraysize; ++i)
+    {
+        scanf("%d",&array[i]);
+    }
+    qsort(array,arraysize,sizeof(int),compare);    
+    for (int i = 0; i < arraysize; ++i)
+    {
+        printf("%d\n",array[i]);
+    }
+    free(array);
     return 0;
 }
