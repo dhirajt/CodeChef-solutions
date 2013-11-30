e01df4e12966cd731fd11b19c5189d78fcec0564
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-11-30 15:01:26 +0530

    solved cooling in Java

--- easy/cooling/Main.java
+++ easy/cooling/Main.java
@@ -6,10 +6,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import java.util.Arrays;
+
 public class Main{
     
     public static void main(String[] args) throws Exception{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        /* code */     
+        
+        int iterations = Integer.parseInt(reader.readLine());
+        
+        for (int k=0; k<iterations; k++) {
+            int pies = Integer.parseInt(reader.readLine());
+
+            String line1[] = reader.readLine().split(" ");
+            String line2[] = reader.readLine().split(" ");
+
+            int weights[] = new int[pies];
+            int weight_limits[] = new int[pies];
+
+            int count=0;
+
+            for (int i=0; i<pies; ++i) {
+                weights[i] = Integer.parseInt(line1[i]);
+                weight_limits[i] = Integer.parseInt(line2[i]);         
+            }
+
+            Arrays.sort(weights);
+            Arrays.sort(weight_limits);
+
+            for (int j=0; j<pies; ++j) {
+                if (weights[count]<=weight_limits[j]) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
     }
 }
