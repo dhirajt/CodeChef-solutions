716826a8b5225782cba0e6ebbedd7ff1149e7849
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-11-30 17:31:45 +0530

    solved cooling in c

--- easy/cooling/cooling.c
+++ easy/cooling/cooling.c
@@ -3,9 +3,51 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int compare (const void * a, const void * b)
+{
+  return ( *(int*)a - *(int*)b );
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int iterations = 0;
+    scanf("%d",&iterations);
+
+    int pies = 0;
+    int count = 0;
+
+    for (int i = 0; i < iterations; ++i)
+    {   
+        count = 0;
+        scanf("%d",&pies);
+
+        int* weights = (int *) calloc(pies,sizeof(int));
+        int* weight_limits = (int *) calloc(pies,sizeof(int));
+
+        for (int j = 0; j < pies; ++j)
+        {
+            scanf("%d",&weights[j]);
+        }
+        for (int k = 0; k < pies; ++k)
+        {
+            scanf("%d",&weight_limits[k]);
+        }
+
+        qsort(weights,pies,sizeof(int),compare);
+        qsort(weight_limits,pies,sizeof(int),compare);
+
+        for (int t=0; t<pies; ++t) 
+        {
+            if (weights[count]<=weight_limits[t])
+            {
+                count++;
+            }
+        }
+        printf("%d\n",count);
+
+    }
+
     return 0;
 }
