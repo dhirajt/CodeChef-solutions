0f2b78967ca212e8dfb6d762bb9f96ff830ff6e2
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-12-08 23:51:11 +0530

    solved cleaningup in Java and C

--- easy/cleaningup/Main.java
+++ easy/cleaningup/Main.java
@@ -10,6 +10,38 @@ public class Main{
     
     public static void main(String[] args) throws Exception{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        /* code */     
+        int iterations = Integer.parseInt(reader.readLine());
+
+        int total = 0;
+        int done = 0;
+        for (int i=0; i<iterations; ++i) {
+            String inputs[] = reader.readLine().split(" ");
+            String chef = "";
+            String assistant = "";
+            total = Integer.parseInt(inputs[0]);
+            done = Integer.parseInt(inputs[1]);
+
+            int tasks[] = new int[total+1];
+            inputs = reader.readLine().split(" ");
+
+            for (int j=0; j<done; ++j) {
+                tasks[Integer.parseInt(inputs[j])] = -1;
+            }
+
+            for (int k=1,t=0; k<total+1; ++k) {
+                if(tasks[k]==-1){
+                    continue;
+                }
+                if(t%2==0){
+                    chef += Integer.toString(k)+" ";
+                }
+                else {
+                    assistant += Integer.toString(k)+" ";
+                }
+                ++t;
+            }
+        System.out.println(chef);
+        System.out.println(assistant);
+        }    
     }
 }
--- easy/cleaningup/cleaningup.c
+++ easy/cleaningup/cleaningup.c
@@ -3,9 +3,72 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void flushbuf()
+{
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    continue;
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int iterations = 0;
+    scanf("%d",&iterations);
+
+    int ntotal = 0;
+    int ndone = 0;
+
+    for (int i = 0; i < iterations; ++i)
+    {
+        scanf("%d %d",&ntotal,&ndone);
+        flushbuf();
+        int* chef = (int *) calloc(ntotal+1,sizeof(int));
+        int* assistant = (int *) calloc(ntotal+1,sizeof(int));
+        int done = 0;
+
+        for (int j = 1; j <= ndone; ++j)
+        {   
+            scanf("%d",&done);
+            chef[done] = -1;
+            assistant[done] = -1;
+        }
+        for (int k = 1,m=0; k <= ntotal; ++k)
+        {   
+            if (chef[k]==-1)
+            {
+                continue;
+            }
+            if (m%2==0)
+            {
+                chef[k] = 1;
+            }
+            else
+            {
+                assistant[k] = 1;
+            }
+            m++;
+        }
+        
+        for (int l = 1; l <= ntotal; ++l)
+        {
+            if (chef[l]>0)
+            {
+                printf("%d ",l);
+            }
+        }
+        printf("\n");
+        for (int m = 1; m <= ntotal; ++m)
+        {
+            if (assistant[m]>0)
+            {
+                printf("%d ",m);
+            }
+        }
+        printf("\n");
+        free(chef);
+        free(assistant);
+    }
     return 0;
 }
